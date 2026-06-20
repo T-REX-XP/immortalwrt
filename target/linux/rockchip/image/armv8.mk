c4afaa0fd6115a3fafdddfbf4259a3f55b6c7cf1
T-REX-XP <invader.darkzone@gmail.com>
2026-06-20 22:14:14 +0200

    Remove button scripts and related configurations for Orange Pi CM5 Base
    This commit deletes the button event scripts for the MaskROM and USERKEY buttons, as well as the associated UCI defaults script and device tree entries. The changes include:
    
    - Removal of `/etc/rc.button/BTN_2` and `/etc/rc.button/wps` scripts.
    - Deletion of the user key handling script in `/etc/rc.wps/50-cm5-userkey`.
    - Updates to the UCI defaults script to ensure the button event log is created without the previous scripts.
    - Adjustments in the device tree patch to remove button configurations.
    
    These changes streamline the button handling process by eliminating unused scripts and configurations.
    
    Signed-off-by: [Your Name] <your.email@example.com>
    Signed-off-by: T-REX-XP <invader.darkzone@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -622,7 +622,7 @@ define Device/xunlong_orangepi-cm5-base
 	fantastic-packages-feeds transmission luci-app-transmission docker dockerd luci-app-docker \
 	luci-app-dockerman kmod-input-adc-keys kmod-button-hotplug kmod-wireguard wireguard-tools luci-proto-wireguard rpcd-mod-wireguard \
 	kmod-amneziawg amneziawg-tools luci-proto-amneziawg usb-modeswitch usbutils kmod-mt76x2u \
-	kmod-rtl8812au-ct wpad-openssl hostapd-utils luci-app-wol etherwake pbr luci-app-pbr \
+	kmod-rtl8812au-ct wpad-openssl hostapd-utils cm5-button-scripts luci-app-wol etherwake pbr luci-app-pbr \
 	luci-app-travelmate travelmate luci-app-fwknopd fwknopd luci-app-privoxy privoxy luci-app-ksmbd \
 	ksmbd-server luci-app-aria2 aria2 luci-app-minidlna minidlna luci-app-statistics collectd \
 	luci-app-nlbwmon nlbwmon luci-app-watchcat watchcat luci-app-sqm sqm-scripts luci-app-ttyd ttyd \
