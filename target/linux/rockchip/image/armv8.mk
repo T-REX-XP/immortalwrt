1af3461ff0b71df022a420a1b6efefc96cb59fda
T-REX-XP <invader.darkzone@gmail.com>
2026-06-21 11:05:15 +0200

    Update device packages for Orange Pi CM5 Base in armv8.mk
    This commit removes the 'luci-app-adblock' package from the device configuration for the Orange Pi CM5 Base, streamlining the package list. The change aims to enhance the overall performance and user experience by eliminating unnecessary packages while retaining essential functionalities.
    
    Signed-off-by: [Your Name] <your.email@example.com>
    Signed-off-by: T-REX-XP <invader.darkzone@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -617,7 +617,7 @@ define Device/xunlong_orangepi-cm5-base
   UBOOT_DEVICE_NAME := orangepi-5-rk3588s
   DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-r8125 luci-ssl luci-mod-network luci-mod-status \
 	luci-mod-dashboard iwinfo rpcd-mod-iwinfo tailscale cloudflared luci-app-tailscale-community \
-	luci-app-cloudflared adblock luci-app-adblock blocky luci-app-blocky luci-app-security-guide \
+	luci-app-cloudflared blocky luci-app-blocky luci-app-security-guide \
 	luci-app-peripherals luci-app-buttons luci-app-oled i2c-tools coreutils-nohup speedtest-go luci-app-speedtest fantastic-keyring \
 	fantastic-packages-feeds transmission luci-app-transmission docker dockerd luci-app-docker \
 	luci-app-dockerman kmod-input-adc-keys kmod-button-hotplug kmod-wireguard wireguard-tools luci-proto-wireguard rpcd-mod-wireguard \
